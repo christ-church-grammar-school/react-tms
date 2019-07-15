a6d4534d4f7d8f7b211cedb7528a66648b696e5b
Max Godfrey <mwugodfrey@gmail.com>
2019-07-15 21:35:39 +0800

    Add header

---
+++ src/header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <nav className="Header">
+        <h1>react-tms</h1>
+      </nav>
+    );
+  }
+}
+
+export default Header;
