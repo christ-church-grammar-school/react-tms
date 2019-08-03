9e62b4448c948c4275c1b1a97d99a466feab9f28
Max Godfrey <mwugodfrey@gmail.com>
2019-08-03 16:38:49 +0800

    Authenticate users with firebase

--- react-tms/src/Firebase/firebase.js
+++ react-tms/src/Firebase/firebase.js
@@ -1,11 +1,21 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 
 import firebaseConfig from './firebase_config';
 
 class Firebase {
   constructor() {
     app.initializeApp(firebaseConfig);
+    this.auth = app.auth;
   }
+
+  // Authentication api
+
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth().signInWithEmailAndPassword(email, password);
+
+  doSignOut = () =>
+    this.auth().signOut();
 }
 
 export default Firebase;
--- react-tms/src/login/Login.js
+++ react-tms/src/login/Login.js
@@ -1,56 +1,75 @@
 import React from 'react';
+import { compose } from 'recompose';
+
+import { withFirebase } from '../Firebase';
 import './Login.css';
 
-class Login extends React.Component {
+const INITIAL_STATE = {
+  authError: false,
+  email: '',
+  password: '',
+};
+
+class LoginBase extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      username: '',
-      password: '',
-    };
+    this.state = {...INITIAL_STATE};
 
-    this.handleUsernameChange = this.handleUsernameChange.bind(this);
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.cancelAuthError = this.cancelAuthError.bind(this);
+    this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    
-    this.failedAuthentication = false;
   }
 
-  handleUsernameChange(event) {
-    this.setState({username: event.target.value});
+  cancelAuthError() {
+    this.setState({authError: false});
   }
 
-  handlePasswordChange(event) {
-    this.setState({password: event.target.value});
+  handleChange(event) {
+    this.setState({[event.target.name]: event.target.value});
   }
 
   handleSubmit(event) {
-    if (this.state.username === '' || this.state.password === '') {
-      this.failedAuthentication = true;
-      event.preventDefault();
-      this.forceUpdate();
-      return;
-    }
-    // TODO(maxgodfrey2004): Implement this function (use props?)
+    const loginComponent = this;
+    const { email, password } = this.state;
+    this.props.firebase
+      .doSignInWithEmailAndPassword(email, password)
+      .then(function() {
+        alert('login successful!');
+        loginComponent.setState({...INITIAL_STATE});
+      }).catch(function(error) {
+        loginComponent.setState({authError: true});
+        loginComponent.refs.emailInput.value = '';
+        loginComponent.refs.passwordInput.value = '';
+      });
+    event.preventDefault();
   }
 
   render() {
+    const { authError } = this.state;
+
     return (
       <div className="Login">
         <h1>Log in</h1>
         <form className="form-main" onSubmit={this.handleSubmit}>
           <fieldset className="form-group">
-            {this.failedAuthentication === true &&
+            {authError &&
               <div className="auth-fail">
                 <p>Authentication Failed.</p>
+                <button onClick={this.cancelAuthError}>
+                  ✖
+                </button>
               </div>
             }
             <input className="form-input"
-                   onChange={this.handleUsernameChange}
-                   placeholder="Username"
+                   name="email"
+                   onChange={this.handleChange}
+                   placeholder="Email"
+                   ref="emailInput"
                    type="text" />
             <input className="form-input"
-                   onChange={this.handlePasswordChange}
+                   name="password"
+                   onChange={this.handleChange}
+                   ref="passwordInput"
                    placeholder="Password"
                    type="password" />
             <button className="form-btn" type="submit">
@@ -62,5 +81,9 @@ class Login extends React.Component {
     );
   }
 }
+
+const Login = compose(
+  withFirebase
+)(LoginBase);
 
 export default Login;
