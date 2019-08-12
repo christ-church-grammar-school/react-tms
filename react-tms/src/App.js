e411c952bd2666ebd4349391a5013f5323c67e75
Max Godfrey <mwugodfrey@gmail.com>
2019-08-12 12:17:18 +0800

    Add firebase sessions

--- react-tms/src/App.js
+++ react-tms/src/App.js
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import Header from './header/Header.js';
-import Login from './login/Login.js';
+import Header from './header/Header';
+import Login from './login/Login';
+import { withFirebase } from './Firebase';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Header />
-        <Switch>
-          <Route exact path="/" />
-          <Route path="/login" component={Login} />
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      authUser: null,
+    };
+  }
+
+  componentDidMount() {
+    const appComponent = this;
+    this.props.firebase.auth().onAuthStateChanged(user => {
+      user
+        ? appComponent.setState({ authUser: user })
+        : appComponent.setState({ authUser: null });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Header authUser={this.state.authUser} />
+          <Switch>
+            <Route exact path="/" />
+            <Route path="/login" component={Login} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withFirebase(App);
--- react-tms/src/header/Header.js
+++ react-tms/src/header/Header.js
@@ -2,16 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
-class Header extends React.Component {
-  render() {
-    return (
-      <nav className="Header">
-        <Link to="/">
-          <h1>react-tms</h1>
-        </Link>
-      </nav>
-    );
-  }
-}
+const Header = ({ authUser }) => (
+  <div>{authUser ? <HeaderAuthorized /> : <HeaderUnauthorized />}</div>
+)
+
+const HeaderAuthorized = () => (
+  <nav className="Header">
+    <Link to="/">
+      <h1>react-tms</h1>
+    </Link>
+  </nav>
+)
+
+const HeaderUnauthorized = () => (
+  <nav className="Header">
+    <Link to="/">
+      <h1>react-tms</h1>
+    </Link>
+    <Link to="/login">
+      <h1>Log in</h1>
+    </Link>
+  </nav>
+)
 
 export default Header;
