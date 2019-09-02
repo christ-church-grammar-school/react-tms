702fe62c264b0eaeea740ef4fc70d0a7025c6cd4
Max Godfrey <mwugodfrey1@gmail.com>
2019-09-02 10:34:56 +0800

    Implement teaching group table

--- react-tms/src/home/Home.js
+++ react-tms/src/home/Home.js
@@ -2,10 +2,21 @@ import React from 'react';
 
 import { withAuthorization } from '../Session';
 
+import './Home.css';
+
 const HomePage = () => (
   <div>
     <h1>Dashboard</h1>
     <p>This page is only accessible by every signed in user.</p>
+
+    <table className="teaching-group-table">
+      <tr>
+        <h3>Your Classes:</h3>
+      </tr>
+      <tr>
+        <p>10ASD1_2019</p>
+      </tr>
+    </table>
   </div>
 )
 
