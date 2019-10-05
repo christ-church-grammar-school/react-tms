cb41ce5d5f6fcbdea0ad9cfb8ec226b922899cce
Max Godfrey <mwugodfrey1@gmail.com>
2019-10-05 21:00:10 +0800

    Write to the Cloud Firestore when questions are saved

--- react-tms/src/Firebase/firebase.js
+++ react-tms/src/Firebase/firebase.js
@@ -1,5 +1,6 @@
 import app from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/storage';
 
 import firebaseConfig from './firebase_config';
 
@@ -7,6 +8,8 @@ class Firebase {
   constructor() {
     app.initializeApp(firebaseConfig);
     this.auth = app.auth;
+    this.db = app.database;
+    this.storage = app.storage;
   }
 
   // Authentication api
@@ -16,6 +19,25 @@ class Firebase {
 
   doSignOut = () =>
     this.auth().signOut();
+
+  // Storage api
+
+  // Returns a Cloud Firestore ref object pertaining to the file/folder within
+  // the firestore at a given path.
+  //
+  // Args:
+  //   path: The aforementioned path which 
+  //
+  getStorageRef(path) {
+    const storageRootRef = this.storage().ref();
+    return storageRootRef.child(path);
+  }
+
+  // Returns a reference to the root of the Cloud Firestore.
+  //
+  getStorageRoot() {
+    return this.storage().ref();
+  }
 }
 
 export default Firebase;
