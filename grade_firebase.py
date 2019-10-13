ce6102cb832f1a36cf8156d18296fce32bb784d4
Angus Finch <developer.finchie@gmail.com>
2019-10-13 12:39:01 +0800

    Migrate inputs & outputs to Firebase storage

--- grade_firebase.py
+++ grade_firebase.py
@@ -22,6 +22,32 @@ def update_mark(transaction, grading_ref, result):
         u'graded': True
     })
 
+def get_blobs(path):
+    files = []
+    for blob in BUCKET.list_blobs(prefix=path):
+        name = blob.name
+        if name[-1] != '/':
+            files.append(name)
+    return files
+
+def download_io(test_name, question_name):
+    path = 'tests/{}/{}'.format(test_name, question_name)
+    inputPath = '{}/inputs/'.format(path)
+    outputPath = '{}/outputs/'.format(path)
+    inputs = []
+    outputs = []
+
+    for path in get_blobs(inputPath):
+        if path[-1] != '/':
+            blob = BUCKET.get_blob(path).download_as_string().decode('utf-8')
+            inputs.append(blob)
+    for path in get_blobs(outputPath):
+        if path[-1] != '/':
+            blob = BUCKET.get_blob(path).download_as_string().decode('utf-8')
+            outputs.append(blob)
+
+    return (inputs, outputs)
+
 def grade_file_url(url, inputs, outputs):
     download = BUCKET.get_blob(url).download_as_string().decode('utf-8')
     filename = url.split('/')[-1]
@@ -47,7 +73,7 @@ def mark_student(student, class_name, inputs, outputs):
                     class_name, student_upload_str))
                         .document(u'test_1'), result)
 
-def mark_class(class_name):
+def mark_class(class_name, test_name, question_name):
     class_ref = DB.collection(u'classes/{}/uploads'.format(class_name))
     class_stream = class_ref.stream()
     test_ref = DB.collection(u'classes/{}/tests'.format(class_name))
@@ -56,8 +82,7 @@ def mark_class(class_name):
     for upload in class_stream:
         for test in test_stream:
             data = test.to_dict()
-            inputs = data['input']
-            outputs = data['output']
+            inputs, outputs = download_io(test_name, question_name)
             mark_student(upload, class_name, inputs, outputs)
 
-mark_class('10ASD1_2019')
+mark_class('10ASD1_2019', 'DemoTest', 'Only Four')
